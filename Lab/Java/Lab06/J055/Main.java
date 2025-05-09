fdaa3d3333bc874bd6c719687c8d1de10756aba4
potterLim <potterLim0808@gmail.com>
2025-05-09 16:44:26 +0900

    feat: add source and header files for Lab 06 - J055

---
+++ Lab/Java/Lab06/J055/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BMICalculator bmiCalculator = new BMICalculator();
+        bmiCalculator.readUserData();
+        bmiCalculator.displayBmi();
+    }
+}
